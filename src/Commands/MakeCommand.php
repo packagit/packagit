7665d17ea65fd72e0e767ca64e23733afc40d1ec
Qisen <yinqisen@gmail.com>
2022-05-28 14:57:33 +0800

    composer dumpautoload after created

--- src/Commands/MakeCommand.php
+++ src/Commands/MakeCommand.php
@@ -77,6 +77,9 @@ class MakeCommand extends Command
         $this->generateFolders();
         $this->generateFiles();
 
+        // composer dumpautoload
+        @exec('composer dumpautoload');
+
         $this->info("Package [{$this->packageName}] created successfully");
     }
 
